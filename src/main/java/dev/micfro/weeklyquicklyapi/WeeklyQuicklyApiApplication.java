342464b19280a146d04c9722b9b2e4272e170626
micfro <michalfrost71@gmail.com>
2024-03-18 12:44:14 +0100

    Initial commit

---
+++ src/main/java/dev/micfro/weeklyquicklyapi/WeeklyQuicklyApiApplication.java
@@ -0,0 +1,13 @@
+package dev.micfro.weeklyquicklyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyQuicklyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeeklyQuicklyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/micfro/weeklyquicklyapi/WeeklyQuicklyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.micfro.weeklyquicklyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyQuicklyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
